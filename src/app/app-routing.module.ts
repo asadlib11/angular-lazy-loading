f7df4cbd30f502cfde281aa4f693a1cda9e9b499
asadlib11 <asadalibaloch059@gmail.com>
2020-02-25 15:58:58 +0500

    added route for modules in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homepage', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+  { path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'settings', loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
